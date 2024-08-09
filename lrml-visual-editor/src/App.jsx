629741176c444b3543aa986c85df858391cd546f
Trevor <tpo920@aucklanduni.ac.nz>
2024-08-09 23:05:26 +1200

    feat/flydown testing

--- lrml-visual-editor/src/App.jsx
+++ lrml-visual-editor/src/App.jsx
@@ -20,6 +20,7 @@ import DarkTheme from '@blockly/theme-dark';
 import "./blockly/blockGenerator";
 import "./blockly/extensions/validators";
 import "./blockly/blocks";
+import { Flydown } from './blockly/flydown/flydown';
 // MUI _________________________________________
 import { Box } from "@mui/material";
 // CSS _________________________________________
@@ -28,6 +29,7 @@ import './App.css';
 function App() {
   const [ws, setWs] = useState(null);
   const [blockCode, setBlockCode] = useState("");
+  const [thisFlydown, setThisFlydown] = useState("");
   const [clause, setClause] = useState("");
   const blocklyRef = useRef(null);
   const { mode } = useContext(ColourModeContext);
@@ -91,8 +93,70 @@ function App() {
       // Initialise the content highlight plugin
       const contentHighlight = new ContentHighlight(ws);
       contentHighlight.init();
+
+      ws.addChangeListener(onBlockClickListener);
+      initBlocklyWithFlydown(ws);
     }
   }, [ws]);
+
+  function onBlockClickListener(event) {
+    if (event.type == Blockly.Events.CLICK) {
+      if (event.targetType == "block") {
+        const block = ws.getBlockById(event.blockId);
+        if (block.isInFlyout || !block.isMovable())
+          return;
+
+        const now = Date.now();
+
+        if (now - (block["_lastClickTime"] ?? 0) < 300) {
+          // Do something
+          console.log("Double click");
+          openFlydown(block);
+
+        }
+
+        block["_lastClickTime"] = now;
+      }
+    }
+  };
+
+  // Initialize Blockly and create a vertical flydown
+  function initBlocklyWithFlydown(workspace) {
+    const flydown = new Flydown(
+      new Blockly.Options({
+        scrollbars: false,
+        rtl: workspace.RTL,
+        renderer: workspace.options.renderer,
+        rendererOverrides: workspace.options.rendererOverrides,
+        parentWorkspace: workspace,
+      })
+    );
+    // ***** [lyn, 10/05/2013] NEED TO WORRY ABOUT MULTIPLE BLOCKLIES! *****
+    workspace.flydown_ = flydown;
+    Blockly.utils.dom.insertAfter(flydown.createDom('g'),
+      workspace.svgBubbleCanvas_);
+    flydown.init(workspace);
+    flydown.autoClose = true; // Flydown closes after selecting a block
+  }
+
+  function openFlydown(currentBlock) {
+    const flydown = Blockly.common.getMainWorkspace().flydown_;
+
+    const scale = flydown.targetWorkspace.scale;
+    flydown.workspace_.setScale(scale);
+
+    const blocksXML2 = ['<xml>' + '<block type="atom_block"></block>' + '<block type="fun_block"></block>' + '</xml>'];
+    const blocksDom = Blockly.utils.xml.textToDom(blocksXML2);
+    const blocksXMLList = blocksDom.children;
+
+    // Calc position
+    const pos = currentBlock.getRelativeToSurfaceXY();
+    pos.x += pos.x * scale;
+    pos.y += pos.y * scale;
+    console.log(pos);
+
+    flydown.showAt(blocksXMLList, pos.x, pos.y);
+  }
 
   // Set theme
   useEffect(() => {
