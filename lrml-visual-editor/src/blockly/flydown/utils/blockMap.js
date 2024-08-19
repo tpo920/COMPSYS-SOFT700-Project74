eb6c1571ba25f6ff9907eb5202e41049c0c2e5b4
Trevor <tpo920@aucklanduni.ac.nz>
2024-08-19 18:01:03 +1200

    feat/Updated block map for block toolbox changes

--- lrml-visual-editor/src/blockly/flydown/utils/blockMap.js
+++ lrml-visual-editor/src/blockly/flydown/utils/blockMap.js
@@ -2,10 +2,12 @@
 export const blockMap = {
     // If
     if_block: ['and_block', 'or_block', 'expr_block', 'atom_block'],
+    then_block: ['and_block', 'or_block', 'expr_block', 'atom_block'],
     // Operators
     and_block: ['expr_block'],
     or_block: ['expr_block'],
     not_block: ['expr_block'],
+    operator_block: [],
     // Expression
     expr_block: ['fun_block', 'atom_block', 'data_block'],
     // Atom
@@ -14,21 +16,13 @@ export const blockMap = {
     fun_block: [],
     rel_block: [],
     var_block: [],
-    ind_block: [],
     // Data
-    data_block: [],
+    data_block: ['baseunit_block', 'prefix_block', 'kind_block'],
     // Deontic
     obligation_block: ['and_block', 'or_block', 'expr_block'],
     permission_block: ['and_block', 'or_block', 'expr_block'],
     prohibition_block: ['and_block', 'or_block', 'expr_block'],
-    right_block: ['and_block', 'or_block', 'expr_block'],
-    violation_block: ['and_block', 'or_block', 'expr_block'],
-    compliance_block: ['and_block', 'or_block', 'expr_block'],
     // Statement
-    constitutive_block: ['if_block'],
-    prescriptive_block: ['if_block'],
-    override_block: ['if_block'],
-    penalty_block: ['if_block'],
-    reparation_block: ['if_block'],
-    factual_block: ['if_block'],
+    rulestatement_block: ['if_block', 'then_block'],
+    appliedstatement_block: ['if_block', 'then_block'],
 };
